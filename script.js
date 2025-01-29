583cc3b38a314bd61542f3342b6a1ef2916e9ced
Laziza <laziza@.com>
2025-01-29 13:51:02 +0300

    portfolio_header

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
-let dark=false;
-let i=10;
-function change_Color(){
-    dark=!dark;
-    console.log(dark)
-}
-document.documentElement.style.setProperty('--txt-color','#fff');
-function change_Color(){
-    let tYil=document.getElementById("inId").value;
-    const vaqt= new Date ();
-    const yil=vaqt.getFullYear();
-    console.log(yil-tYil)
-}
-setInterval(()=>{
-    // i=i+1;
-    // console.log(i);
-    if(i>0){
-        i==10
+let menuOpen = false;
+function dropDown() {
+    menuOpen = !menuOpen;
+    if (menuopen == true) {
+        document.getElementById("navId").style.display = "block";
     }
-    i=i-1;
-        console.log(i);
-},200)
+    else {
+        document.getElementById("navId").style.display = "none";
+    }
+}
